28114ac7cb3ec04ea82f1ea2c00dcd7dc3951c8a
pramath6095 <97220563+pramath6095@users.noreply.github.com>
2023-11-01 00:22:29 +0530

    Update project school.py

--- project school.py
+++ project school.py
@@ -7,83 +7,85 @@ cur=con.cursor()
 
 
 
-def admin_login():#print(' '*(50-len('')//2),
+
+
+def admin_login():#print(' '*(60-len('')//2),
     print()
-    print('='*100)
+    print('='*120)
     print()
-    print(' '*(50-len('ADMIN LOGIN')//2),'ADMIN LOGIN')
+    print(' '*(60-len('ADMIN LOGIN')//2),'ADMIN LOGIN')
     print()
-    print(' '*(50-len('Enter your credentials')//2),'Enter your Credentials')
-    print(' '*(50-len('Enter your credentials')//2),'ID : ',end='')
+    print(' '*(60-len('Enter your credentials')//2),'Enter your Credentials')
+    print(' '*(60-len('Enter your credentials')//2),'ID : ',end='')
     id=input()
-    print(' '*(50-len('Enter your credentials')//2),'PASSWORD : ',end='')
+    print(' '*(60-len('Enter your credentials')//2),'PASSWORD : ',end='')
     pwd=input()
     print()
-    print('='*100)
+    print('='*120)
     cur.execute("select * from admin_login")
     data=cur.fetchall()
     for i in data:
         if i[0]==id and i[1]==pwd:
             print()
-            print('*'*100)
-            print(' '*(50-len('login successful')//2),"Login Successful!")
-            print('*'*100)
+            print('*'*120)
+            print(' '*(60-len('login successful')//2),"Login Successful!")
+            print('*'*120)
             print()
             #admin_interface()
             break
     else:
         print()
-        print('*'*100)
-        print(' '*(50-len('login failed')//2),"Login Failed!")
-        print(' '*(50-len('Returning to Main Menu')//2),"Returning to Main Menu")
-        print('*'*100)
+        print('*'*120)
+        print(' '*(60-len('login failed')//2),"Login Failed!")
+        print(' '*(60-len('Returning to Main Menu')//2),"Returning to Main Menu")
+        print('*'*120)
         print()
 
 def doctor_login():
-    print('='*100)
+    print('='*120)
     print()
-    print(' '*(50-len('DOCTOR LOGIN')//2),'DOCTOR LOGIN')
+    print(' '*(60-len('DOCTOR LOGIN')//2),'DOCTOR LOGIN')
     print()
-    print(' '*(50-len('Enter your credentials')//2),'Enter your Credentials')
-    print(' '*(50-len('Enter your credentials')//2),'ID : ',end='')
+    print(' '*(60-len('Enter your credentials')//2),'Enter your Credentials')
+    print(' '*(60-len('Enter your credentials')//2),'ID : ',end='')
     id=input()
-    print(' '*(50-len('Enter your credentials')//2),'PASSWORD : ',end='')
+    print(' '*(60-len('Enter your credentials')//2),'PASSWORD : ',end='')
     pwd=input()
     print()
-    print('='*100)
+    print('='*120)
     cur.execute("select * from doctor_login")
     data=cur.fetchall()
     for i in data:
         if i[0]==id and i[1]==pwd:
             print()
-            print('*'*100)
-            print(' '*(50-len('login successful')//2),"Login Successful!")
-            print('*'*100)
+            print('*'*120)
+            print(' '*(60-len('login successful')//2),"Login Successful!")
+            print('*'*120)
             print()
             #doctor_interface(id)
             break
     else:
         print()
-        print('*'*100)
-        print(' '*(50-len('login failed!')//2),"Login Failed!")
-        print(' '*(50-len('Returning to Main Menu')//2),"Returning to Main Menu")
-        print('*'*100)
+        print('*'*120)
+        print(' '*(60-len('login failed!')//2),"Login Failed!")
+        print(' '*(60-len('Returning to Main Menu')//2),"Returning to Main Menu")
+        print('*'*120)
         print()
     
 def user_page():
     while True:
 
         print()
-        print('='*100)
+        print('='*120)
         print()
-        print(' '*(50-len('USER (SIGNIN/LOGIN)')//2),'USER (SIGNIN/LOGIN)')
+        print(' '*(60-len('USER (SIGNIN/LOGIN)')//2),'USER (SIGNIN/LOGIN)')
         print()
-        print(' '*(50-len('1. Sign In To Your Account')//2),'1. Sign In To Your Account')
-        print(' '*(50-len('1. Sign In To Your Account')//2),'2. Dont have an account? Sign up')
-        print(' '*(50-len('1. Sign In To Your Account')//2),'3. Exit to Main Menu')
+        print(' '*(60-len('1. Sign In To Your Account')//2),'1. Sign In To Your Account')
+        print(' '*(60-len('1. Sign In To Your Account')//2),'2. Dont have an account? Sign up')
+        print(' '*(60-len('1. Sign In To Your Account')//2),'3. Exit to Main Menu')
         print()
-        print(' '*(50-len('Enter your Choice')//2),'Enter your Choice')
-        print('='*100)
+        print(' '*(60-len('Enter your Choice')//2),'Enter your Choice')
+        print('='*120)
 
         try:
             ch=int(input())
@@ -97,51 +99,51 @@ def user_page():
                 break
             else:
                 print()
-                print('*'*100)
-                print(' '*(50-len('INVALID CHOICE!!')//2),'INVALID CHOICE!!')
-                print('*'*100)
+                print('*'*120)
+                print(' '*(60-len('INVALID CHOICE!!')//2),'INVALID CHOICE!!')
+                print('*'*120)
                 print()
 
         except ValueError:
             print()
-            print('*'*100)
-            print(' '*(50-len('The choice has to be an integer!!!')//2),'THE CHOICE HAS TO BE AN INTEGER!!!')
-            print('*'*100)
+            print('*'*120)
+            print(' '*(60-len('The choice has to be an integer!!!')//2),'THE CHOICE HAS TO BE AN INTEGER!!!')
+            print('*'*120)
             print()
 
 
 def user_login():
     print()
-    print('='*100)
+    print('='*120)
     print()
-    print(' '*(50-len('USER LOGIN')//2),'USER LOGIN')
+    print(' '*(60-len('USER LOGIN')//2),'USER LOGIN')
     print()
-    print(' '*(50-len('Enter your credentials')//2),'Enter your Credentials')
-    print(' '*(50-len('Enter your credentials')//2),'ID : ',end='')
+    print(' '*(60-len('Enter your credentials')//2),'Enter your Credentials')
+    print(' '*(60-len('Enter your credentials')//2),'ID : ',end='')
     id=input()
-    print(' '*(50-len('Enter your credentials')//2),'PASSWORD : ',end='')
+    print(' '*(60-len('Enter your credentials')//2),'PASSWORD : ',end='')
     pwd=input()
     print()
-    print('='*100)
+    print('='*120)
     print()
     cur.execute("select * from user_login")
     data=cur.fetchall()
     for i in data:
         if i[0]==id and i[1]==pwd:
             print()
-            print('*'*100)
-            print(' '*(50-len('login successful')//2),"Login Successful!")
-            print('*'*100)
+            print('*'*120)
+            print(' '*(60-len('login successful')//2),"Login Successful!")
+            print('*'*120)
             print()
             #doctor_interface(id)
             break
     else:
         print()
-        print('*'*100)
-        print(' '*(50-len('login failed!')//2),"Login Failed!")
-        print(' '*(50-len('Contact Admin to Recover Account')//2),"Contact Admin to Recover Account")
-        print(' '*(50-len('Returning to Main Menu')//2),"Returning to Main Menu")
-        print('*'*100)
+        print('*'*120)
+        print(' '*(60-len('login failed!')//2),"Login Failed!")
+        print(' '*(60-len('Contact Admin to Recover Account')//2),"Contact Admin to Recover Account")
+        print(' '*(60-len('Returning to Main Menu')//2),"Returning to Main Menu")
+        print('*'*120)
         print()
 
 
@@ -155,20 +157,20 @@ def user_login():
 def menu_page():
     while True:
         print()
-        print('='*100)
+        print('='*120)
         print()
 
-        print(' '*(50-len('MAIN MENU')//2),'MAIN MENU')
+        print(' '*(60-len('MAIN MENU')//2),'MAIN MENU')
         print()
-        print(' '*(50-len('1. Admin(login)')//2),'1. Admin(login)')
-        print(' '*(50-len('1. Admin(login)')//2),'2. Doctor(login)')
-        print(' '*(50-len('1. Admin(login)')//2),'3. User(signin/login)')
-        print(' '*(50-len('1. Admin(login)')//2),'4. Exit')
+        print(' '*(60-len('1. Admin(login)')//2),'1. Admin(login)')
+        print(' '*(60-len('1. Admin(login)')//2),'2. Doctor(login)')
+        print(' '*(60-len('1. Admin(login)')//2),'3. User(signin/login)')
+        print(' '*(60-len('1. Admin(login)')//2),'4. Exit')
 
         print()
-        print(' '*(50-len('Enter your Choice')//2),'Enter your Choice')
+        print(' '*(60-len('Enter your Choice')//2),'Enter your Choice')
         print()
-        print('='*100)
+        print('='*120)
 
     
         try:
@@ -182,24 +184,24 @@ def menu_page():
                 user_page()
             elif ch==4:
                 print()
-                print('='*100)
-                print(' '*(50-len('THANK YOU!!!')//2),'THANK YOU!!!')
-                print('='*100)
+                print('='*120)
+                print(' '*(60-len('THANK YOU!!!')//2),'THANK YOU!!!')
+                print('='*120)
                 break
             else:
                 print()
-                print('*'*100)
-                print(' '*(50-len('INVALID CHOICE!!')//2),'INVALID CHOICE!!')
-                print('*'*100)
+                print('*'*120)
+                print(' '*(60-len('INVALID CHOICE!!')//2),'INVALID CHOICE!!')
+                print('*'*120)
                 print()
                 
             
 
         except ValueError:
             print()
-            print('*'*100)
-            print(' '*(50-len('The choice has to be an integer!!!')//2),'THE CHOICE HAS TO BE AN INTEGER!!!')
-            print('*'*100)
+            print('*'*120)
+            print(' '*(60-len('The choice has to be an integer!!!')//2),'THE CHOICE HAS TO BE AN INTEGER!!!')
+            print('*'*120)
             print()
             
 
@@ -207,51 +209,53 @@ def menu_page():
 
 
 def welcome_page():
-    
-    print('='*100)
-    print()
-    print(' '*(50-len('WELCOME TO NAGRAJU HOSPITAL')//2),'WELCOME TO NAGRAJU HOSPITAL')
-    print()
-    print(' '*(50-len('1. Main Menu')//2),'1. Main Menu')
-    print(' '*(50-len('1. Main Menu')//2),'2. Exit')
-    print()
-
-    print(' '*(50-len('Enter your choice')//2),'Enter your choice')
-    print()
-    print('='*100)
-    
     while True:
+        print('='*120)
+        print()
+        print(' '*(60-len('WELCOME TO NAGRAJU HOSPITAL')//2),'WELCOME TO NAGRAJU HOSPITAL')
+        print()
+        print(' '*(60-len('1. Main Menu')//2),'1. Main Menu')
+        print(' '*(60-len('1. Main Menu')//2),'2. Exit')
+        print()
+
+        print(' '*(60-len('Enter your choice')//2),'Enter your choice')
+        print()
+        print('='*120)
+        
+        
         try:
             ch=int(input())
             if ch==1:
                 menu_page()
+                break
             elif ch==2:
                 print()
-                print('='*100)
-                print(' '*(50-len('THANK YOU!!!')//2),'THANK YOU!!!')
-                print('='*100)
+                print('='*120)
+                print(' '*(60-len('THANK YOU!!!')//2),'THANK YOU!!!')
+                print('='*120)
                 print()
                 break
+            
             else:
                 
                 print()
-                print('*'*100)
-                print(' '*(50-len('INVALID CHOICE!!')//2),'INVALID CHOICE!!')
-                print('*'*100)
+                print('*'*120)
+                print(' '*(60-len('INVALID CHOICE!!')//2),'INVALID CHOICE!!')
+                print('*'*120)
                 print()
-                welcome_page()
-            break  
+                #welcome_page()
+            
 
         except ValueError:
             print()
-            print('*'*100)
-            print(' '*(50-len('The choice has to be an integer!!!')//2),'THE CHOICE HAS TO BE AN INTEGER!!!')
+            print('*'*120)
+            print(' '*(60-len('The choice has to be an integer!!!')//2),'THE CHOICE HAS TO BE AN INTEGER!!!')
             
-            print('*'*100)
+            print('*'*120)
             print()
             
-            welcome_page()
-
+            #welcome_page()
+        
 
 
 
